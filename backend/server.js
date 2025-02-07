79d6990f1a78409cf3a15d6c70a6632050c9a604
Big-Mindset <wadoodmemon0@gmail.com>
2025-02-07 07:18:07 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,9 @@ let PORT  = process.env.PORT || 5000
 let __dirname = path.resolve()
 app.use(express.json({limit : "10mb"}))
 app.use(cookieParser())
+
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : process.env.ClIENT_URL,
     credentials : true
 }))
 app.use("/api/auth",route)
